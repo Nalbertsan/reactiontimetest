7eba7cfa6b7fdfa8e0f51e3ca5fee2cbf96b9cd1
Nalbert <nalbert1000@gmail.com>
2024-05-25 02:32:58 -0300

    Create page.tsx

---
+++ app/movie/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React from "react";
+
+const Movie: React.FC = () => { 
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Video Embed Example</h1>
+        <div>
+          <iframe
+            id="myIframe"
+            src="https://embedder.net/e/634649"
+            width="1500"
+            height="500"
+            title="Embedded Video"
+            style={{ position: 'absolute', top: 0, left: 0 }}
+          ></iframe>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Movie;
